38f74edc8b2befd3bdcfdd5cb37f560b9e1cbad0
Kyle Martin <kyle.martin.0819@gmail.com>
2024-01-29 23:32:43 -0500

    init: created header, intro section, stats section, and about section.

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: ['./**/*.{html,js}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                body: [
+                    'Lato',
+                    'ui-sans-serif',
+                    'system-ui',
+                    '-apple-system',
+                    'system-ui',
+                    'Segoe UI',
+                    'Roboto',
+                    'Helvetica Neue',
+                    'Arial',
+                    'Noto Sans',
+                    'sans-serif',
+                    'Apple Color Emoji',
+                    'Segoe UI Emoji',
+                    'Segoe UI Symbol',
+                    'Noto Color Emoji',
+                ],
+                sans: [
+                    'Lato',
+                    'ui-sans-serif',
+                    'system-ui',
+                    '-apple-system',
+                    'system-ui',
+                    'Segoe UI',
+                    'Roboto',
+                    'Helvetica Neue',
+                    'Arial',
+                    'Noto Sans',
+                    'sans-serif',
+                    'Apple Color Emoji',
+                    'Segoe UI Emoji',
+                    'Segoe UI Symbol',
+                    'Noto Color Emoji',
+                ],
+            },
+        },
+    },
+    plugins: [],
+};
